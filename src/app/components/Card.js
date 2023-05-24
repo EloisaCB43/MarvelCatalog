c742fa95e3a609cb52f626011ce4f7e2fed6ec94
Eloisa Cardona <eloisacardona43@gmail.com>
2023-05-23 23:01:30 -0500

    Card ready

--- src/app/components/Card.js
+++ src/app/components/Card.js
@@ -1,25 +1,36 @@
 import Image from "next/image";
 
-function Card() {
+function Card(props) {
+  const { name, comics, movies, avatar } = props;
   return (
-    <div className="w-[248px] h-[448px] border-t-[0.75px] rounded-[5px] border-solid border-[#5B5A56] flex flex-col justify-between items-center relative bg-[#1E2328] p-[35px] gap-5">
-      <Image src="/card_container.png" fill className="" />
-      <div className="w-[179px] h-[18px] relative flex justify-center items-center">
-        <Image src="/name_decorators.png" fill />
+    <div className="w-[248px] h-[448px] border-t-[0.75px] rounded-[5px] flex flex-col justify-between items-center relative p-[35px] gap-5 bg-[url(/card_container.png)]">
+      <div className="w-[179px] h-[18px] relative flex justify-center items-center bg-[url(/name_decorators.png)]">
         <h2 className="text-center text-[#F0E6D2] text-[14px] tracking-[1PX] w-[81px]">
-          PERSONAJE NAME
+          {name}
         </h2>
       </div>
-      <div className="w-[164px] h-[190px] relative hover:animate-[spin_6s_linear_infinite]">
-        <Image src="/avatar_container.png" fill />
+      <div className="w-[164px] h-[190px] relative grid place-items-center">
+        <Image
+          src="/avatar_container.png"
+          fill
+          className="hover:animate-[spin_6s_linear_infinite]"
+        />
+        <Image
+          className="rounded-full border border-[#3C3C41]"
+          src={avatar || "/default_avatar.png"}
+          width={125}
+          height={125}
+        />
       </div>
 
-      <div className="border rounded-[5px] border-solid border-[#5B5A56] text-[#F0E6D2] bg-[#1E2328] w-[178px] h-[49px] py-2.5 px-5">
-        <h3 className="text-[16px] font-['Spiegel']">Comics: 25</h3>
-      </div>
-      <div className="border rounded-[5px] border-solid border-[#5B5A56] text-[#F0E6D2] bg-[#1E2328] w-[178px] h-[49px] py-2.5 px-5">
-        <h3 className="text-[16px] font-['Spiegel']">Movies: 25</h3>
-      </div>
+      <h3 className="border rounded-[5px] border-solid border-[#5B5A56] text-[#F0E6D2] bg-[#1E2328] w-[178px] h-[49px] py-1 px-5 text-base text-center">
+        <span className="font-sans align-middle">Comics:</span>{" "}
+        <span className="text-3xl align-middle pl-3">{comics}</span>
+      </h3>
+      <h3 className="border rounded-[5px] border-solid border-[#5B5A56] text-[#F0E6D2] bg-[#1E2328] w-[178px] h-[49px] py-1 px-5 text-base text-center">
+        <span className="font-sans align-middle">Movies:</span>{" "}
+        <span className="text-3xl align-middle pl-3">{movies}</span>
+      </h3>
     </div>
   );
 }
--- src/app/page.js
+++ src/app/page.js
@@ -4,7 +4,7 @@ import Card from "./components/Card";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Card />
+      <Card name="NAME" comics={25} movies={25} />
     </main>
   );
 }
